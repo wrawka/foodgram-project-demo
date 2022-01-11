e4b497b63fc403769f4ecccb586a1936fec489ce
wrawka <wrawka@gmail.com>
2022-01-11 17:40:26 +0300

    some models ddone

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,16 +1,36 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=7, null=True)
+    slug = models.SlugField(null=True)
+
 
 class Ingredient(models.Model):
     name = models.CharField()
     measurement_unit = models.CharField()
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Recipe(models.Model):
-    name = models.CharField()
-    ingredients = ...
-    tags = ...
-    image = ...
+    name = models.CharField(max_length=200)
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    tags = models.ManyToManyField(Tag)
+    image = models.CharField()
     text = models.TextField()
-    cooking_time = ...
+    cooking_time = models.IntegerField(
+        validators=[MinValueValidator(1)]
+    )
+
+    def __str__(self) -> str:
+        return self.name
 
 
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.IntegerField()
