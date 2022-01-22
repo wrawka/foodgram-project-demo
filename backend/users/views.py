3aaeb4362998f24f3b9436be537d7f057f2d2eba
wrawka <wrawka@gmail.com>
2022-01-22 15:29:11 +0300

    subscriptions implemented

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,60 +1,69 @@
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers, status
 from rest_framework.decorators import action
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Follow
-from .serializers import FollowSerializer, FoodgramUserSerializer
+from .serializers import SubscriptionUserSerializer, FoodgramUserSerializer
 
 
 User = get_user_model()
 
 class FollowViewSet(GenericViewSet):
-    serializer_class = FollowSerializer
+    serializer_class = SubscriptionUserSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [SearchFilter]
-    search_fields = ['following__username', 'user__username']
+    search_fields = ['author__username', 'user__username']
     pagination_class=LimitOffsetPagination
 
-    def get_queryset(self):
-        return Follow.objects.filter(user=self.request.user)
+
+    @action(detail=False)
+    def subscriptions(self, request, *args, **kwargs):
+        following = User.objects.filter(followers__user=request.user)
+        page = self.paginate_queryset(following)
+        if page is not None:
+            serializer = SubscriptionUserSerializer(page, context={'request': request}, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = SubscriptionUserSerializer(following, context={'request': request}, many=True)
+        return Response(serializer.data)
+
 
     @action(detail=True)
     def subscribe(self, request, pk):
-        following_user = get_object_or_404(User, id=pk)
-        serializer = self.get_serializer(data={'user': request.user.id, 'following': following_user.id})
-        if serializer.is_valid():
-            serializer.save(
-                user=self.request.user,
-                following=following_user
+        author = get_object_or_404(User, id=pk)
+        if author == request.user:
+            return Response(
+                {'errors': 'Can\'t follow self.'},
+                status=status.HTTP_400_BAD_REQUEST
             )
-            some_data = serializer.data
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+        try:
+            Follow.objects.create(user=request.user, author=author)
+        except IntegrityError:
+            return Response(
+                {'errors': 'Already subscribed to this user.'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        serializer = self.get_serializer(author)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
     @subscribe.mapping.delete
     def unsubscribe(self, request, pk):
-        following_instance = get_object_or_404(
-            Follow,
-            user=request.user.id, 
-            following=get_object_or_404(User, id=pk).id)
-        following_instance.delete()
+        author=get_object_or_404(User, id=pk)
+        try:           
+            Follow.objects.get(
+                user=request.user.id, 
+                author=author.id
+            ).delete()
+        except Follow.DoesNotExist:
+            return Response(
+                {'errors': 'Not following this author.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(detail=False)
-    def subscriptions(self, request):
-        following = request.user.following.all()
-        page = self.paginate_queryset(following)
-        if page is not None:
-            serializer = FollowSerializer(page, context={'request': request}, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = FollowSerializer(following, context={'request': request}, many=True)
-        return Response(serializer.data)
